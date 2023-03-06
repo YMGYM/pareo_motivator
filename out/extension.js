503c58eea93d7cec58a9536e9e5db0559fc8b380
YMGYM <good_day1234@naver.com>
2023-03-07 00:33:04 +0900

    change picture based on user behavior

--- out/extension.js
+++ out/extension.js
@@ -9,20 +9,25 @@ function activate(context) {
 exports.activate = activate;
 class PareoViewProvider {
     constructor() {
-        this._imageSrc = {
+        this.imageSrc = {
             'idle': '<div class="tenor-gif-embed" data-postid="25561754" data-share-method="host" data-aspect-ratio="1.29555" data-width="100%"><a href="https://tenor.com/view/pareo-bandor%C4%B1-bang-dream-gif-25561754">Pareo Bandorı GIF</a>from <a href="https://tenor.com/search/pareo-gifs">Pareo GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>',
             'working': '<div class="tenor-gif-embed" data-postid="18252669" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%"><a href="https://tenor.com/view/bangdream-bandori-garupapico-anime-pareo-gif-18252669">Bangdream Bandori GIF</a>from <a href="https://tenor.com/search/bangdream-gifs">Bangdream GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>',
             'rest': '<div class="tenor-gif-embed" data-postid="23805459" data-share-method="host" data-aspect-ratio="1.79775" data-width="100%"><a href="https://tenor.com/view/bang-dream-bandori-garupa-pico-garupa-pico-fever-anime-gif-23805459">Bang Dream Bandori GIF</a>from <a href="https://tenor.com/search/bang+dream-gifs">Bang Dream GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
         };
-        this._imageKey = 'idle';
+        this.imageKey = 'idle';
+        this.restCounter = 0;
     }
     resolveWebviewView(webviewView, context, token) {
         this._view = webviewView;
         webviewView.webview.options = {
             enableScripts: true,
         };
-        const timeout = setInterval(() => this._changeKey(), 10000);
-        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
+        const timeout = setInterval(() => this._changeKey(), 5000);
+        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this.imageKey));
+        // when user work, reset rest counter
+        vscode.workspace.onDidChangeTextDocument((e) => {
+            this.restCounter = 0;
+        });
     }
     _getHtmlForWebview(imageSrc) {
         return `
@@ -39,28 +44,23 @@ class PareoViewProvider {
       </html>`;
     }
     _selectImageSrc(imageKey) {
-        return this._imageSrc[imageKey];
+        return this.imageSrc[imageKey];
     }
     _changeKey() {
-        // 순환해서 키 변경 (idle -> working -> rest -> idle)
-        switch (this._imageKey) {
-            case 'idle':
-                this._imageKey = 'working';
-                this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-                break;
-            case 'working':
-                this._imageKey = 'rest';
-                this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-                break;
-            case 'rest':
-                this._imageKey = 'idle';
-                this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-                break;
-            default:
-                this._imageKey = 'idle';
-                this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-                break;
+        this.restCounter += 1;
+        console.log(this.restCounter);
+        if (this.restCounter >= 60) {
+            // when user rest more then 300 seconds, change image to idle
+            this.imageKey = 'idle';
         }
+        else if (this.restCounter >= 6) {
+            // when user rest more then 30 seconds, change image to rest
+            this.imageKey = 'rest';
+        }
+        else {
+            this.imageKey = 'working';
+        }
+        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this.imageKey));
     }
 }
 PareoViewProvider.viewType = 'pareoMotivator';
--- src/extension.ts
+++ src/extension.ts
@@ -12,13 +12,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 class PareoViewProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = 'pareoMotivator';
-  public readonly _imageSrc = {
+  public readonly imageSrc:Object = {
     'idle': '<div class="tenor-gif-embed" data-postid="25561754" data-share-method="host" data-aspect-ratio="1.29555" data-width="100%"><a href="https://tenor.com/view/pareo-bandor%C4%B1-bang-dream-gif-25561754">Pareo Bandorı GIF</a>from <a href="https://tenor.com/search/pareo-gifs">Pareo GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>',
     'working': '<div class="tenor-gif-embed" data-postid="18252669" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%"><a href="https://tenor.com/view/bangdream-bandori-garupapico-anime-pareo-gif-18252669">Bangdream Bandori GIF</a>from <a href="https://tenor.com/search/bangdream-gifs">Bangdream GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>',
     'rest': '<div class="tenor-gif-embed" data-postid="23805459" data-share-method="host" data-aspect-ratio="1.79775" data-width="100%"><a href="https://tenor.com/view/bang-dream-bandori-garupa-pico-garupa-pico-fever-anime-gif-23805459">Bang Dream Bandori GIF</a>from <a href="https://tenor.com/search/bang+dream-gifs">Bang Dream GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
   };
 
-  public _imageKey = 'idle';
+  public imageKey = 'idle';
+  public restCounter = 0;
 
   private _view?: vscode.WebviewView;
 
@@ -33,8 +34,14 @@ class PareoViewProvider implements vscode.WebviewViewProvider {
       enableScripts: true,
     };
 
-    const timeout = setInterval(() => this._changeKey(), 10000);
-    this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
+    const timeout = setInterval(() => this._changeKey(), 5000);
+    this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this.imageKey));
+
+    // when user work, reset rest counter
+    vscode.workspace.onDidChangeTextDocument((e) => {
+      this.restCounter = 0;
+    });
+
   }
 
   private _getHtmlForWebview(imageSrc: string) {
@@ -52,33 +59,28 @@ class PareoViewProvider implements vscode.WebviewViewProvider {
       </html>`;
   }
 
-  private _selectImageSrc(imageKey: string) {
-    return this._imageSrc[imageKey];
+  private _selectImageSrc(imageKey: string):string {
+    return this.imageSrc[imageKey as keyof typeof this.imageSrc];
   }
 
   private _changeKey() {
-    // 순환해서 키 변경 (idle -> working -> rest -> idle)
-    switch (this._imageKey) {
-      case 'idle':
-        this._imageKey = 'working';
-        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-        break;
-      case 'working':
-        this._imageKey = 'rest';
-        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-        break;
-      case 'rest':
-        this._imageKey = 'idle';
-        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-        break;
-      default:
-        this._imageKey = 'idle';
-        this._view.webview.html = this._getHtmlForWebview(this._selectImageSrc(this._imageKey));
-        break;
-      
-      
+    this.restCounter += 1;
+    console.log(this.restCounter);
+    if (this.restCounter >= 60) {
+      // when user rest more then 300 seconds, change image to idle
+      this.imageKey = 'idle';
     }
+    else if (this.restCounter >= 6){
+      // when user rest more then 30 seconds, change image to rest
+      this.imageKey = 'rest';
+    }
+    else {
+      this.imageKey = 'working';
+    }
+
+    this._view!.webview.html = this._getHtmlForWebview(this._selectImageSrc(this.imageKey));
   }
 
+  
 }
 export function deactivate() {}
